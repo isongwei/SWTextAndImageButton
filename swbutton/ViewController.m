cc6e4badba77e826eba8bb613e93a9561fa3fdeb
iSong <i_songwei@163.com>
2017-04-18 13:15:55 +0800

    SWTextAndImageButton
    init

--- swbutton/ViewController.m
+++ swbutton/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import "SWTextAndImageButton.h"
 @interface ViewController ()
 
 @end
@@ -16,13 +16,40 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
+    
+    SWTextAndImageButton * btn =  [[SWTextAndImageButton alloc]initWithFrame:(CGRectMake(100, 10, 200, 100))];
+    btn.backgroundColor =[ UIColor redColor];
+    
+    btn.imageView.backgroundColor = [UIColor greenColor];
+    btn.titleLabel.backgroundColor = [UIColor blueColor];
+    btn.rectType =SWButtonTypeTextLeft;
+    btn.contentDistance = 10;
+    btn.padding = 20;
+    
+    [btn setTitle:@"sadsad" forState:(UIControlStateNormal)];
+    
+    [btn setTitleColor:[UIColor blackColor] forState:(UIControlStateNormal)];
+    [btn setImage:[UIImage imageNamed:@"00.png"] forState:(UIControlStateNormal)];
+    
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+    NSLog(@"%@", NSStringFromUIEdgeInsets(btn.titleEdgeInsets));
+    NSLog(@"%@", NSStringFromUIEdgeInsets(btn.imageEdgeInsets));
+    NSLog(@"%@", NSStringFromUIEdgeInsets(btn.contentEdgeInsets));
+    
+    
+    
+    NSLog(@"----");
+    NSLog(@"%@", NSStringFromCGRect(btn.titleLabel.frame));
+    NSLog(@"%@", NSStringFromCGRect(btn.imageView.frame));
+    
+    
+    
+    
+    
+    [self.view addSubview:btn];
+    
+    
 }
 
 
